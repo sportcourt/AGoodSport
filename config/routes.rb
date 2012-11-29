6da5518406a4719df0b47fd6b375abff27dc9f64
Courtney Gray <scourtneygray@gmail.com>
2012-11-29 13:07:37 -0600

    Initial authentication attempt

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ AGoodSport::Application.routes.draw do
   resources :charities
   
   resources :organizations
+  
+  resources :users
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
